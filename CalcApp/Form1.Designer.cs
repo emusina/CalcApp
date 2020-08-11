fffb62a400abea0ea3f24282e4a17028540e5d75
Elena M <emusina@users.noreply.github.com>
2020-08-11 22:21:45 +0300

    added buttons

--- CalcApp/Form1.Designer.cs
+++ CalcApp/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CalcApp
 {
-    partial class Form1
+    partial class FormHomepage
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ButtonCalculatorForm = new System.Windows.Forms.Button();
+            this.ButtonAboutForm = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ButtonCalculatorForm
+            // 
+            this.ButtonCalculatorForm.Location = new System.Drawing.Point(426, 106);
+            this.ButtonCalculatorForm.Name = "ButtonCalculatorForm";
+            this.ButtonCalculatorForm.Size = new System.Drawing.Size(98, 49);
+            this.ButtonCalculatorForm.TabIndex = 0;
+            this.ButtonCalculatorForm.Text = "Calculator";
+            this.ButtonCalculatorForm.UseVisualStyleBackColor = true;
+            // 
+            // ButtonAboutForm
+            // 
+            this.ButtonAboutForm.Location = new System.Drawing.Point(426, 199);
+            this.ButtonAboutForm.Name = "ButtonAboutForm";
+            this.ButtonAboutForm.Size = new System.Drawing.Size(98, 53);
+            this.ButtonAboutForm.TabIndex = 1;
+            this.ButtonAboutForm.Text = "About";
+            this.ButtonAboutForm.UseVisualStyleBackColor = true;
+            // 
+            // FormHomepage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(962, 459);
+            this.Controls.Add(this.ButtonAboutForm);
+            this.Controls.Add(this.ButtonCalculatorForm);
+            this.Name = "FormHomepage";
+            this.Text = "Homepage";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button ButtonCalculatorForm;
+        private System.Windows.Forms.Button ButtonAboutForm;
     }
 }
 
--- CalcApp/Form1.cs
+++ CalcApp/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace CalcApp
 {
-    public partial class Form1 : Form
+    public partial class FormHomepage : Form
     {
-        public Form1()
+        public FormHomepage()
         {
             InitializeComponent();
         }
